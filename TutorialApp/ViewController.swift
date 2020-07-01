7b48762be09d3c71521a80792397437cdfa6356f
Keegan Brown <keegqbrown@gmail.com>
2020-06-30 20:50:28 -0500

    added segue

--- TutorialApp/ViewController.swift
+++ TutorialApp/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var mainTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = mainTableView.dequeueReusableCell(withIdentifier: "cellid")
+        cell?.textLabel?.text = "Test"
+        return cell!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mainTableView.delegate = self
+        mainTableView.dataSource = self
+        
     }
 
 
