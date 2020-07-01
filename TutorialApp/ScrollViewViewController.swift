efbdf7a589fd79bc523ba380b5436fe484e52f28
Carly Cameron <cmcameron@unomaha.edu>
2020-06-30 21:03:44 -0500

    update scrollview

---
+++ TutorialApp/ScrollViewViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ScrollViewViewController.swift
+//  TutorialApp
+//
+//  Created by Carly Cameron on 6/30/20.
+//  Copyright © 2020 Keegan Brown. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewViewController: UIViewController {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        descriptionLabel.text = "class ScrollViewViewController: UIViewController { @IBOutlet weak var descriptionLabel: UILabel! override func viewDidLoad() { super.viewDidLoad() class ScrollViewViewController: UIViewController { @IBOutlet weak var descriptionLabel: UILabel! override func viewDidLoad() { super.viewDidLoad() class ScrollViewViewController: UIViewController { @IBOutlet weak var descriptionLabel: UILabel! override func viewDidLoad() { super.viewDidLoad() class ScrollViewViewController: UIViewController { @IBOutlet weak var descriptionLabel: UILabel! override func viewDidLoad() { super.viewDidLoad()"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
